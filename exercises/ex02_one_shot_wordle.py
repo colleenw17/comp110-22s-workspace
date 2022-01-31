b4cf23a840f0cb3d78892a4453865b0c89eb861d
Colleen Whitener <cw17@live.unc.edu>
2022-01-31 13:08:20 -0500

    changed Lint erros in ex2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""One Shot - One step towards Wordle"""
+"""One Shot - One step towards Wordle."""
 
 __author__ = "730319000"
 
@@ -12,27 +12,27 @@ guess: str = ""
 exists: bool = False
 alt: int = 0
 
-while len(intro) != len(SECRET):  #the length of my guess is not equal to the length of the answer so I have to try again
+while len(intro) != len(SECRET):  # the length of my guess is not equal to the length of the answer so I have to try again
     intro = input("That was not 6 letters! Try again: ")
 
-while i < len(SECRET): #when 0 < length of the answer and if the first index = to the answer index then a green box appears
+while i < len(SECRET):  # when 0 < length of the answer and if the first index = to the answer index then a green box appears
     if intro[i] == SECRET[i]:
         guess = guess + GREEN_BOX
     else: 
         exists = False
         alt = 0
-        while exists != True and alt < len(SECRET): #if the first index does not equal the answer index, and it doesn't exist in the answer then move ov
-            if intro[i] == SECRET[alt]: #the letter is found in the answer in the correct spot so we can stop
+        while exists is not True and alt < len(SECRET):  # if the first index does not equal the answer index, and it doesn't exist in the answer then move ov
+            if intro[i] == SECRET[alt]:  # the letter is found in the answer in the correct spot so we can stop
                 exists = True
             alt = alt + 1
-        if exists == False: #the letter is not found in the answer so we stop and print a white box
+        if exists is False:  # the letter is not found in the answer so we stop and print a white box
             guess = guess + WHITE_BOX
         else:
             guess = guess + YELLOW_BOX
     i = i + 1
 print(guess)
 
-if intro == SECRET: #if we guess the answer we stop playing and if we don't guess the answer but it is 6 letters we stop
+if intro == SECRET:  # if we guess the answer we stop playing and if we don't guess the answer but it is 6 letters we stop
     print("Woo! You got it!")
 else:
     print("Not quite. Play again soon!")
